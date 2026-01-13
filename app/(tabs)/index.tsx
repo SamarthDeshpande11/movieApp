cbba4da2786e0c1104949dcbb03fed505bea24dc
SamarthDeshpande11 <sam.deshpande1135@gmail.com>
2026-01-13 23:18:13 +0530

    search bar and navigation bar added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,33 @@
-import { Text, View } from "react-native";
+import { Image,ScrollView,Text, View } from "react-native";
 import {Link} from "expo-router"
-
+import {images} from "@/constants/images"
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
 import "@/global.css"
-export default function index() {
+import { icons } from "@/constants/icons";
+
+
+
+
+export default function Index() {
+
+  const router=useRouter();
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-dark-200 font-bold">
-        Movie App
-      </Text>
-      
+    <View className="flex-1 bg-primary"><Image source={images.bg} className="absolute w-full z-0"/>
+    <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{
+      minHeight:"100%",paddingBottom:10
+    }}>
+      <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+      <View className="flex-1 mt-5">
+        <SearchBar 
+            onPress={()=>router.push("/search")}
+            placeholder="Search for a movie"
+          
+        
+        />
+    </View>
+    </ScrollView>
+    
     </View>
   );
 }
