72e06679d55026daa2b5db1d395ae6ae309f749d
SamarthDeshpande11 <sam.deshpande1135@gmail.com>
2026-01-14 22:04:23 +0530

    movie card trending movies added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
-import { Image,ScrollView,Text, View } from "react-native";
+import { ActivityIndicator, Image,ScrollView,Text, View } from "react-native";
 import {Link} from "expo-router"
 import {images} from "@/constants/images"
 import SearchBar from "@/components/SearchBar";
 import { useRouter } from "expo-router";
 import "@/global.css"
 import { icons } from "@/constants/icons";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import { FlatList } from "react-native";
+import MovieCard from "@/components/MovieCard";
 
 
 
@@ -12,22 +16,60 @@ import { icons } from "@/constants/icons";
 export default function Index() {
 
   const router=useRouter();
+
+  const{
+    data:movies,
+    loading:moviesLoading,
+    error:moviesError
+  }=useFetch(()=>fetchMovies({
+    query:''
+  }))
   return (
     <View className="flex-1 bg-primary"><Image source={images.bg} className="absolute w-full z-0"/>
     <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{
       minHeight:"100%",paddingBottom:10
     }}>
       <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-      <View className="flex-1 mt-5">
+      {moviesLoading?(
+        <ActivityIndicator 
+          size="large"
+          color="#0000ff"
+          className="mt-10 self-center"
+        />
+      ):moviesError?(
+        <Text>Error:{moviesError?.message}</Text>
+      ):(
+        <View className="flex-1 mt-5">
         <SearchBar 
             onPress={()=>router.push("/search")}
-            placeholder="Search for a movie"
-          
-        
+            placeholder="Search for a movie"        
         />
+        <>
+          <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+
+          <FlatList 
+            data={movies}
+            renderItem={({item})=>(
+              <MovieCard
+                {...item}
+              />
+            )}
+            keyExtractor={(item)=>item.id.toString()}
+            numColumns={3}
+            columnWrapperStyle={{
+              justifyContent:'flex-start',
+              gap:20,
+              paddingRight:5,
+              marginBottom:10
+            }}
+            className="mt-2 pb-32"
+            scrollEnabled={false}
+          />
+        </>
     </View>
-    </ScrollView>
-    
+      )}
+      
+    </ScrollView>    
     </View>
   );
 }
--- constants/icons.ts
+++ constants/icons.ts
@@ -1,19 +1,10 @@
-import home from "@/assets/icons/home.png";
-import search from "@/assets/icons/search.png";
-import person from "@/assets/icons/person.png";
-import logo from "@/assets/icons/logo.png";
-import save from "@/assets/icons/save.png";
-import star from "@/assets/icons/star.png";
-import play from "@/assets/icons/play.png";
-import arrow from "@/assets/icons/arrow.png";
-
 export const icons = {
-  home,
-  search,
-  person,
-  logo,
-  save,
-  star,
-  play,
-  arrow,
+  home: require("../assets/icons/home.png"),
+  search: require("../assets/icons/search.png"),
+  person: require("../assets/icons/person.png"),
+  logo: require("../assets/icons/logo.png"),
+  save: require("../assets/icons/save.png"),
+  star: require("../assets/icons/star.png"),
+  play: require("../assets/icons/play.png"),
+  arrow: require("../assets/icons/arrow.png"),
 };
