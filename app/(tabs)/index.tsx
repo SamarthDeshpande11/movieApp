8ed9dca1f197d344a2239b3b2efa4712c148c6a9
SamarthDeshpande11 <sam.deshpande1135@gmail.com>
2026-01-15 01:01:28 +0530

    added serachpage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,12 +17,13 @@ export default function Index() {
 
   const router=useRouter();
 
-  const{
+  const{ 
     data:movies,
     loading:moviesLoading,
     error:moviesError
   }=useFetch(()=>fetchMovies({
     query:''
+  
   }))
   return (
     <View className="flex-1 bg-primary"><Image source={images.bg} className="absolute w-full z-0"/>
